1738497edef015caaed5015daedaf0464e0bbeb6
nickschmitt <nickschmitt@gmail.com>
2021-02-04 08:56:46 -0800

    triggering a double fault with an unhandled page fault

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,10 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // written to trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     #[cfg(test)]
     test_main();
