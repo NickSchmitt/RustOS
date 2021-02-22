4cf707a3fb00d7f3a0d11e162e768360df79d8a6
nickschmitt <nickschmitt@gmail.com>
2021-02-21 21:11:40 -0800

    calling init_idt from main.rs with an init function in lib.rs

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,7 @@ pub extern "C" fn _start() -> ! {
     blog_os::init();
 
     // written to trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    };
+    x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
