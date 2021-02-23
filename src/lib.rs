42550c2a39a9c6602c5f1c3ad1554a484b60b185
nickschmitt <nickschmitt@gmail.com>
2021-02-22 18:03:15 -0800

    Double Fault Handling: Creating a Task State Segment that contains a separate double fault stack in its IST.

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ use core::panic::PanicInfo;
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
+pub mod gdt;
 
 pub trait Testable {
     fn run(&self) -> ();
--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,12 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    // trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    };
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // trigger a stack overflow
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
