b850d9087199cc6b5d576bee53702e2aaa81bcff
nickschmitt <nickschmitt@gmail.com>
2021-02-24 22:11:54 -0800

    Double fault stack overflow integration test interrupt descriptor table (IDT) implementation

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -2,8 +2,10 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::InterruptDescriptorTable;
 
-use blog_os::{gdt, test_panic_handler, serial_print};
+use blog_os::{gdt::{self, DOUBLE_FAULT_IST_INDEX}, serial_print, test_panic_handler};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -16,6 +18,22 @@ pub extern "C" fn _start() -> ! {
 	stack_overflow();
 
 	panic!("Execution continued after stack overflow");
+}
+
+lazy_static! {
+	static ref TEST_IDT: InterruptDescriptorTable = {
+		let mut idt = InterruptDescriptorTable::new();
+		unsafe {
+			idt.double_fault
+				.set_handler_fn(test_double_fault_handler)
+				.set_stack_index(blog_os::gdt::DOUBLE_FAULT_IST_INDEX);
+		}
+		idt
+	};
+}
+
+pub fn init_test_idt() {
+	TEST_IDT.load();
 }
 
 #[allow(unconditional_recursion)]
