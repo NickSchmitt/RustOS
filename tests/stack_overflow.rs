8eb97656b7a38601bc0cc08f3ec80805603a20d6
nickschmitt <nickschmitt@gmail.com>
2021-02-24 21:54:39 -0800

    Double fault stack overflow integration test _start function implementation

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -3,11 +3,25 @@
 
 use core::panic::PanicInfo;
 
-use blog_os::test_panic_handler;
+use blog_os::{gdt, test_panic_handler, serial_print};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	unimplemented!();
+	serial_print!("stack_overflow::stack_overflow...\t");
+
+	blog_os::gdt::init();
+	init_test_idt();
+
+	//trigger a stack overflow
+	stack_overflow();
+
+	panic!("Execution continued after stack overflow");
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow(){
+	stack_overflow(); // for each recursion, the return address is pushed
+	volatile::Volatile::new(0).read(); // prevent tail recursion optimizations
 }
 
 #[panic_handler]
