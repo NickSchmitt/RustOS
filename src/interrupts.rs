2d01e6e8cb37b2e1a3a9c16c0c8a4de78f67c153
nickschmitt <nickschmitt@gmail.com>
2021-03-04 17:38:48 -0800

    Implementing the 8259 PIC (programmable interrupt controller) for hardware interrupts by remapping PIC interrupts to 32-47 so that they don't overlap with exceptions in the IDT

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,6 +2,8 @@ use crate::println;
 use crate::gdt;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use lazy_static::lazy_static;
+use pic8259_simple::ChainedPics;
+use spin;
 
 
 lazy_static! {
@@ -32,6 +34,17 @@ extern "x86-interrupt" fn double_fault_handler(
 {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> = 
+    spin::Mutex::new(
+        unsafe {
+            ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)
+        }
+    );
+
 
 #[test_case]
 fn test_breakpoint_exception(){
