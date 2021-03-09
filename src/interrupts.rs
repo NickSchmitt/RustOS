88f6c47d617b4f38ce2aabbad277dbad0f0ca80f
nickschmitt <nickschmitt@gmail.com>
2021-03-08 18:55:12 -0800

    Instead of manually converting all keyboard scancodes, we use the pc-keyboard crate for translation. First we create a static Keyboard object protected by a Mutex lock, specifying a US keyboard layout and scancode set 1. Ignore HandleControl to prevent mapping ctrl+[a-z] to Unicode chars. After creating the keyboard object, we specify that on interrupt, we lock the Mutex, read the scancode, then pass it to Option<KeyEvent>, which specifies which key and whether it was a press or release. process_keyevent translates the key event.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -53,37 +53,36 @@ extern "x86-interrupt" fn timer_interrupt_handler(
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(
-    _stack_frame: &mut InterruptStackFrame)
-{
-        use x86_64::instructions::port::Port;
+    _stack_frame: &mut InterruptStackFrame){
+        
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
 
-        let mut port = Port::new(0x60);
-        let scancode: u8 = unsafe { port.read() };
-        let key = match scancode {
-            0x02 => Some('1'),
-            0x03 => Some('2'),
-            0x04 => Some('3'),
-            0x05 => Some('4'),
-            0x06 => Some('5'),
-            0x07 => Some('6'),
-            0x08 => Some('7'),
-            0x09 => Some('8'),
-            0x0a => Some('9'),
-            0x0b => Some('0'),
-            _ => None,
-        };
+    lazy_static!{
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore));
+    }
 
-        if let Some(key) = key {
-            print!("{}", key);
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+    
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
         }
+    }
 
-        unsafe {
-            PICS.lock()
-                .notify_end_of_interrupt(
-                    InterruptIndex::Keyboard.as_u8()
-                );
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(
+                InterruptIndex::Keyboard.as_u8()
+            );
 
-        }
+    }
 }
 
 
