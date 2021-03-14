e28ff023f9c6008075fbbddbf5c9fd5123a5c604
nickschmitt <nickschmitt@gmail.com>
2021-03-13 16:35:11 -0800

    Testing virtual to physical address translation function.

--- src/main.rs
+++ src/main.rs
@@ -18,34 +18,31 @@ entry_point!(kernel_main);
 #[no_mangle] 
 // Entry point, since the linker looks for a function named `_start` by default
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::memory::active_level_4_table;
+    use blog_os::memory::translate_addr;
     use x86_64::VirtAddr;
 
     println!("Hello World{}","!");
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset)};
+    
+    let addresses = [
+        // identity-mapped VGA buffer page
+        0xb8000,
 
-    for (i, entry) in l4_table.iter().enumerate(){
-        if !entry.is_unused() {
-            println!("L4 Entry{}: {:?}", i, entry);
-            
-            // get the entry's physical address
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
+        //some code page
+        0x201008,
 
-            // print non-empty entries of the level 3 table
-            for (i, entry) in l3_table.iter().enumerate(){
-                if !entry.is_unused() {
-                    println!(" L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+        //some stack page
+        0x0100_0020_1a10,
 
+        boot_info.physical_memory_offset,
+    ];
 
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset)};
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     #[cfg(test)]
