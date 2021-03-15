49ea1e12db33ac4eb952a8c13cee43e9df9f1857
nickschmitt <nickschmitt@gmail.com>
2021-03-14 19:20:57 -0700

    Since our EmptyFrameAllocator cannot create additional frames as needed, we'll need to map to an address in which no new frames are needed. Depending on the virtual page chosen, we may need to create level 3, 2, and 1 page tabless. However, we do know that the bootloader loads itself into the first megabyte of virtual address space, so no new page tables are required. The first megabyte of virtual address space won't require additional tables, however, because the bootloader loads itself there. This will therefore work for our EmptyFrameAllocator. We map the page at address 0 to the VGA buffer by passing page, mapper (an Offset Page Table) and our EmptyFrameAllocator into create_example_mapping. The page is then converted to a raw and offset by 400 (to account for the VGA buffer's new line) and we volatile write New! to the VGA buffer.

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ entry_point!(kernel_main);
 // Entry point, since the linker looks for a function named `_start` by default
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use blog_os::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
+    use x86_64::{structures::paging::Page, VirtAddr};
 
     println!("Hello World{}","!");
     blog_os::init();
@@ -27,25 +27,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
 
     // initialize a mapper
-    let mapper = unsafe {memory::init(phys_mem_offset)};
+    let mut mapper = unsafe {memory::init(phys_mem_offset)};
+    let mut frame_allocator = memory::EmptyFrameAllocator;
     
-    let addresses = [
-        // identity-mapped VGA buffer page
-        0xb8000,
-        //some code page
-        0x201008,
-        //some stack page
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    // map unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-
-        // use mapper.translate_addr method
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    
 
     #[cfg(test)]
     test_main();
