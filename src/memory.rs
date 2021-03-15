32f5489929ed88734ddd96cc993fa63be06f2d1c
nickschmitt <nickschmitt@gmail.com>
2021-03-14 18:23:47 -0700

    To create a new mapping, we will use the Mapper trait's map_to function. As an example implementation we create the create_example_mapping function. It takes a page to be mapped, a mutable reference to an OffsetPageTable, and a frame allocator that is generic over all types that impl FrameAllocator, with a generic of Size4KiB. We then pass these to map_to with some flags.

--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,4 @@
-use x86_64::{VirtAddr, structures::paging::{PageTable, OffsetPageTable}, PhysAddr};
+use x86_64::{VirtAddr, PhysAddr, structures::paging::{PageTable, OffsetPageTable, Page, PhysFrame, Mapper, Size4KiB, FrameAllocator}};
 
 pub unsafe fn init(physical_memory_offset: VirtAddr)-> OffsetPageTable<'static> {
 	let level_4_table = active_level_4_table(physical_memory_offset);
@@ -19,3 +19,19 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
 		&mut *page_table_ptr // unsafe
 	}
 
+pub fn create_example_mapping(
+	page: Page,
+	mapper: &mut OffsetPageTable,
+	frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+){
+	use x86_64::structures::paging::PageTableFlags as Flags;
+
+	let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+	let flags = Flags::PRESENT | Flags::WRITABLE;
+	// FIXME: UNSAFE, only for testing.
+	let map_to_result = unsafe {
+		mapper.map_to(page, frame, flags, frame_allocator)
+	};
+
+	map_to_result.expect("map_to failed").flush();
+}
