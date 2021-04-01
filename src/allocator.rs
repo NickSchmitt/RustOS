3ae78f570cab555b388198a4fe96922a838b6236
nickschmitt <nickschmitt@gmail.com>
2021-04-01 14:57:21 -0700

    Defining a virtual memory range, with an address at HEAP_START and a size of 100 sets of 1024 bytes, i.e. 100KiB.

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,6 +1,9 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 
+pub const HEAP_START: usize = 0x_4444_4444_0000;
+pub const HEAP_SIZE: usize = 100 * 1024;
+
 pub struct Dummy;
 
 #[global_allocator]
