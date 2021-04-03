af0573b6fba0c6079d46871a9385007bc35e4c10
nickschmitt <nickschmitt@gmail.com>
2021-04-03 00:36:04 -0700

    Create integration test for new allocator.
    To ensure our allocation code works, we create a new test file, reusing our test running and panic handler from lib.rs, and implementing a similar function to kernel_main, calling test_main unconditionally."

---
+++ tests/heap_allocation.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+
+entry_point!(main);
+
+fn main(boot_info: &'static BootInfo) -> ! {
+	unimplemented!();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+	blog_os::test_panic_handler(info)
+}
