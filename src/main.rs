2d923cc9a9e11b19ed1aea36f451679819ebd6ca
nickschmitt <nickschmitt@gmail.com>
2021-04-03 00:26:53 -0700

    Use allocation and collection types of the alloc crate
    With the allocator initialized, we now demonstrate its use by allocating a value on the heap with Box::new(), we can create a dynamically sized vector with alloc::vec::Vec and  demonstrate a reference counting vector with alloc::rc::Rc. Box and Vec's location on the heap is evidenced by their addresses at 0x_4444_4444_*."

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ extern crate alloc;
 use blog_os::println;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use alloc::boxed::Box;
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
 entry_point!(kernel_main);
 
@@ -46,8 +46,21 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let x = Box::new(41);
-    
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}",&heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+
     #[cfg(test)]
     test_main();
 
